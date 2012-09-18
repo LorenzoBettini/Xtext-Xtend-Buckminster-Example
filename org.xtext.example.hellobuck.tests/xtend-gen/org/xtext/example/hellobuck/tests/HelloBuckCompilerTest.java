8d57db13d47daa9886eb50a50efc85f2bed56245
Lorenzo Bettini <bettini@dsi.unifi.it>
2012-09-19 01:17:07 +0200

    reduced plugins in product.feature

--- org.xtext.example.hellobuck.tests/xtend-gen/org/xtext/example/hellobuck/tests/HelloBuckCompilerTest.java
+++ org.xtext.example.hellobuck.tests/xtend-gen/org/xtext/example/hellobuck/tests/HelloBuckCompilerTest.java
@@ -52,8 +52,8 @@ public class HelloBuckCompilerTest {
         }
       };
     this._compilationTestHelper.compile(_builder, new IAcceptor<Result>() {
-        public void accept(Result arg0) {
-          _function.apply(arg0);
+        public void accept(Result t) {
+          _function.apply(t);
         }
     });
   }
@@ -101,8 +101,8 @@ public class HelloBuckCompilerTest {
         }
       };
     this._compilationTestHelper.compile(_builder, new IAcceptor<Result>() {
-        public void accept(Result arg0) {
-          _function.apply(arg0);
+        public void accept(Result t) {
+          _function.apply(t);
         }
     });
   }
