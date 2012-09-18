442346b319cd9a48a3b90427b8b31c534a64744c
Lorenzo Bettini <bettini@dsi.unifi.it>
2012-09-18 21:08:02 +0200

    changed project wizard contents

--- org.xtext.example.hellobuck.ui/src/org/xtext/example/hellobuck/ui/wizard/HelloBuckProjectCreatorCustom.java
+++ org.xtext.example.hellobuck.ui/src/org/xtext/example/hellobuck/ui/wizard/HelloBuckProjectCreatorCustom.java
@@ -9,20 +9,12 @@ import com.google.common.collect.Lists;
 
 /**
  * @author bettini
- *
+ * 
  */
 public class HelloBuckProjectCreatorCustom extends HelloBuckProjectCreator {
 
 	@Override
 	protected List<String> getRequiredBundles() {
-		return Lists.newArrayList(
-				"com.ibm.icu",
-				"org.eclipse.xtext", 
-				"org.eclipse.xtext.generator",
-				"org.eclipse.xtend",
-				"org.eclipse.xtend.typesystem.emf",
-				"org.eclipse.xpand",
-				"org.eclipse.emf.mwe2.launch;resolution:=optional",
-				DSL_GENERATOR_PROJECT_NAME);
+		return Lists.newArrayList(DSL_GENERATOR_PROJECT_NAME);
 	}
 }
